53d67e173e2aed383536c4855e8f5c9a2e11a980
beauxq <beauxq@yahoo.com>
2023-08-30 16:08:22 -0700

    fix item id list to match data in rom patch

--- worlds/subversion/item.py
+++ worlds/subversion/item.py
@@ -1,13 +1,13 @@
 from collections import defaultdict
-from typing import Iterator, Mapping
+from typing import Dict, Iterator
 from BaseClasses import Item, ItemClassification as IC
 from .config import base_id
 
-from subversion_rando.item_data import Item as SvItem, items_unpackable, Items
+from subversion_rando.item_data import Item as SvItem, Items
 from subversion_rando.fillAssumed import FillAssumed
 
 
-classifications: Mapping[str, IC] = defaultdict(lambda: IC.progression)
+classifications: Dict[str, IC] = defaultdict(lambda: IC.progression)
 classifications.update({
     Items.Refuel[0]: IC.filler,
     Items.SmallAmmo[0]: IC.filler,
@@ -31,10 +31,44 @@ class SubversionItem(Item):
         self.sv_item = id_to_sv_item[code]
 
 
+# The order of this list must match itemnames.asm
+local_id_to_sv_item: Dict[int, SvItem] = {
+    0x00: Items.Energy,
+    0x01: Items.Missile,
+    0x02: Items.Super,
+    0x03: Items.PowerBomb,
+    0x04: Items.Bombs,
+    0x05: Items.Charge,
+    0x06: Items.Ice,
+    0x07: Items.HiJump,
+    0x08: Items.SpeedBooster,
+    0x09: Items.Wave,
+    0x0a: Items.Spazer,
+    0x0b: Items.Speedball,
+    0x0c: Items.Varia,
+    0x0d: Items.Aqua,
+    0x0e: Items.Xray,
+    0x0f: Items.Plasma,
+    0x10: Items.Grapple,
+    0x11: Items.SpaceJump,
+    0x12: Items.Screw,
+    0x13: Items.Morph,
+    0x14: Items.Refuel,
+    0x15: Items.DamageAmp,
+    0x16: Items.SmallAmmo,
+    0x17: Items.LargeAmmo,
+    0x18: Items.AccelCharge,
+    0x19: Items.SpaceJumpBoost,
+    0x1a: Items.GravityBoots,
+    0x1b: Items.DarkVisor,
+    0x1c: Items.MetroidSuit,
+    0x1d: Items.Hypercharge
+}
+""" starting from 0 """
+
 id_to_sv_item = {
-    i + base_id: item
-    for i, item in enumerate(items_unpackable)
-    if item != Items.spaceDrop
+    id_ + base_id: item
+    for id_, item in local_id_to_sv_item.items()
 }
 
 name_to_id = {
