956aad267bdb8899467e12a1e5ba426347a39ca9
beauxq <beauxq@yahoo.com>
2024-08-10 15:53:56 -0700

    name lookups without unique IDs (requires 0.5.0)

--- worlds/subversion/client.py
+++ worlds/subversion/client.py
@@ -144,7 +144,7 @@ class SubversionSNIClient(SNIClient):
             # print(f"{location_id=}")
 
             ctx.locations_checked.add(location_id)
-            location = ctx.location_names[location_id]
+            location = ctx.location_names.lookup_in_game(location_id)
             snes_logger.info(
                 f'New Check: {location} ({len(ctx.locations_checked)}/'
                 f'{len(ctx.missing_locations) + len(ctx.checked_locations)})'
@@ -178,8 +178,11 @@ class SubversionSNIClient(SNIClient):
             snes_buffered_write(ctx, SM_RECV_QUEUE_WCOUNT,
                                 bytes([item_out_ptr & 0xFF, (item_out_ptr >> 8) & 0xFF]))
             logging.info('Received %s from %s (%s) (%d/%d in list)' % (
-                color(ctx.item_names[item.item], 'red', 'bold'),
+                color(ctx.item_names.lookup_in_game(item.item), 'red', 'bold'),
                 color(ctx.player_names[item.player], 'yellow'),
-                ctx.location_names[item.location], item_out_ptr, len(ctx.items_received)))
+                ctx.location_names.lookup_in_slot(item.location, item.player),
+                item_out_ptr,
+                len(ctx.items_received)
+            ))
 
         await snes_flush_writes(ctx)
